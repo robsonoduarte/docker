aa43ff9f10cc2677d1b0f74baa78c8b66859c21f
Robson Duarte <robson.o.d@gmail.com>
2016-05-11 21:51:24 -0300

    add ScheduledThreaPool.

--- docker-scala/src/main/scala/br/com/mystudies/docker/scala/EchoWeb.scala
+++ docker-scala/src/main/scala/br/com/mystudies/docker/scala/EchoWeb.scala
@@ -1,16 +1,17 @@
 package br.com.mystudies.docker.scala
 
 import java.nio.channels.SocketChannel
+
 import java.net.InetSocketAddress
 import java.net.ConnectException
 import java.util.concurrent.Executors
-import java.util.concurrent.TimeUnit
+import java.util.concurrent.TimeUnit._
 
 
 object EchoWeb extends App {
 
-   var ex = Executors newSingleThreadExecutor
+     val ex  = Executors.newScheduledThreadPool(1)
 
-   ex submit(new Echo("http://httpstat.us"))
+     ex scheduleAtFixedRate(Echo("http://httpstat.us") , 0L, 10L, SECONDS)
 
 }
