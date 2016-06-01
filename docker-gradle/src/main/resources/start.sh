38ee1dba9a2ee4ee72719848ac927f797c2c9531
Robson Duarte <robson.o.d@gmail.com>
2016-06-01 11:50:28 -0300

    add start.sh to init the container.

---
+++ docker-gradle/src/main/resources/start.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+JAVA_OPTS="
+-server \
+-Xms56m \
+-Xmx56m \
+-Denvironment=ho \
+-Dcom.sun.management.jmxremote \
+-Dcom.sun.management.jmxremote.port=1098 \
+-Dcom.sun.management.jmxremote.rmi.port=1098 \
+-Dcom.sun.management.jmxremote.authenticate=false \
+-Dcom.sun.management.jmxremote.ssl=false \
+-Djava.rmi.server.hostname=52.200.219.47"
+
+
+java -jar $JAVA_OPTS #archiveName
